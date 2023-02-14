95e8c4fa6b2fe47b8fb26a6ea7941723472a8adb
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-14 11:14:25 -0800

    finalTask

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if((year <= 2023 && year > 0) && (month <=12 && month > 0) && (year % 4 !=0) && (month !=2)){
+            System.out.println(30);
+        } else if ((year <= 2023 && year > 0) && (month <=12 && month > 0) && (year % 4 == 0) && (month == 2)){
+            System.out.println(29);
+        } else if ((year <= 2023 && year > 0) && (month <=12 && month > 0) && (year % 4 == 0) && (month != 2)){
+            System.out.println(30);
+        } else {
+            System.out.println("invalid date");
+        }
 
     }
 }
